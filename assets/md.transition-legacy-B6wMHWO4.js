e3931ee9b9e62c2ccf25e58d20a5918cdce65e29
remultajustinekyle <justinekyleremulta2@gmail.com>
2025-02-11 23:09:04 +0800

    Updates

--- assets/md.transition-legacy-B6wMHWO4.js
+++ assets/md.transition-legacy-B6wMHWO4.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DM-otI_K.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-9nHQaJl2.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
